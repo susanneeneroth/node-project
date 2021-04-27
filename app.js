c8fb42754550f714ca031b6656e40fcf91056843
susanneeneroth <susanne@centimedia.se>
2021-04-27 23:29:31 +0200

    Practice files and code

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200)
-  .json({message: 'Hello from the server node-project', app: 'Susannes node project'});
+// 1) middlewares
+app.use(morgan('dev')); // till loggning
+
+app.use(express.json()); // middleware for POST requests
+
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 🦊');
+  next();
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...')
-})
-
-const port = 3001;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
+
+// 3) Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
