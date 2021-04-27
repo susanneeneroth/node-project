e4935cbcf6e075b13ebab70f6027eed768bc6009
susanneeneroth <susanne@centimedia.se>
2021-04-27 13:41:52 +0200

    Test

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server node-project');
+  res.status(200)
+  .json({message: 'Hello from the server node-project', app: 'Susannes node project'});
 });
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...')
+})
 
 const port = 3001;
 app.listen(port, () => {
